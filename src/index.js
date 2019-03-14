604c787f42565fbbede67ffda859487438491a81
Anudeep Samaiya <anudeepsamaiya@gmail.com>
2019-03-14 23:58:10 +0530

    function components

--- src/index.js
+++ src/index.js
@@ -2,16 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
+function Square(props) {
     return (
-      <button className="square" 
-        onClick={() => this.props.onClick()}
+      <button 
+        className="square" 
+        onClick={props.onClick}
       >
-        {this.props.value}
+        {props.value}
       </button>
     );
-  }
 }
 
 class Board extends React.Component {
